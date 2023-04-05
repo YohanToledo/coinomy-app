7c036d04ddc616ca933cbe5d8ed95d495d0dfbd7
YOHAN GABRIEL TOLEDO BERNART <YGTBERNART@UNIVEL.UNIVEL.BR>
2023-04-04 22:06:45 -0300

    add jwt decode function

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -24,11 +24,24 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
     setIsAuthenticated(false);
   };
 
+  const parseJwt = (token: string) => {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+  }
+
   useEffect(() => {
     const token = localStorage.getItem('coinomy_token');
 
     if (token) {
       setIsAuthenticated(true);
+
+      const decoded = parseJwt(token)
+
       if (window.location.href.slice(-5) === 'login') {
         console.log('redirect')
         navigate("/app/home")
