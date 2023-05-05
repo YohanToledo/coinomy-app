e3f198c2a03a27dd13a6f39a01f8b6b1a4e36665
Anderson Neves <neves1697@gmail.com>
2023-05-04 22:50:19 -0300

    Fix: Adicionado elementos na classe ProfilePreferences, estilizado e começado alguns ajustes no ícone de perfil

--- src/components/ProfilePreferences/index.tsx
+++ src/components/ProfilePreferences/index.tsx
@@ -1,7 +1,8 @@
 import { BiCategory } from "react-icons/bi";
 import { GrLogout } from "react-icons/gr";
 import { ImCoinDollar } from "react-icons/im";
-import { MdAccountBalance, MdOutlineManageAccounts } from "react-icons/md";
+import { MdAccountBalance, MdOutlineManageAccounts, MdAlternateEmail, MdOutlinePassword } from "react-icons/md";
+
 import "../Preferences/Preferences.scss";
 import "./ProfilePreferences.scss";
 import { Link } from "react-router-dom";
@@ -10,62 +11,56 @@ import { Link } from "react-router-dom";
 const ProfilePreferences = () => {
   return (
     <>
+      <h1 id="titulo">Perfil</h1>
+      <div className="options-container options-container-profile">
 
-<div className="options-container">
-        <h1>Perfil</h1>
+
+
+        
+          <div className="option-card profile">
+            <MdOutlineManageAccounts className="icone-minha-profile">
+            </MdOutlineManageAccounts>
+            <input type="file" name="" id="" />
+          </div>
+        
 
         <Link to="profile">
           <div className="option-card minha-conta">
             <MdOutlineManageAccounts className="icone-minha-conta">
             </MdOutlineManageAccounts>
-            Minha Conta
+            Nome Completo
           </div>
         </Link>
 
-        
+
 
         <Link to="app/options/profile">
           <div className="option-card moeda">
-            <ImCoinDollar className="icone-moeda">
-            </ImCoinDollar>
-            Moeda
+            <MdAlternateEmail className="icone-moeda">
+            </MdAlternateEmail>
+            E-mail
           </div>
         </Link>
 
         <Link to="app/options/profile">
           <div className="option-card contas">
-            <MdAccountBalance className="icone-conta">
-            </MdAccountBalance>
-
-            Contas
-
+            <MdOutlinePassword className="icone-conta">
+            </MdOutlinePassword>
+            Senha
           </div>
         </Link>
-
-        <Link to="app/options/profile">
-          <div className="option-card categorias">
-            <BiCategory className="icone-categorias">
-            </BiCategory>
-
-            Categorias
-
-          </div>
-        </Link>
-
 
         <Link to="app/options/profile">
           <div className="option-card voltar">
             <GrLogout className="icone-voltar">
             </GrLogout>
-
             Voltar
-
           </div>
         </Link>
 
 
       </div>
-    
+
     </>
   )
 }
