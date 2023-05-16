6b7b175716ed086155f35d15ddb31bbaee8cf167
ANDERSON DOS SANTOS NEVES <ASNEVES2@UNIVEL.UNIVEL.BR>
2023-05-15 22:02:21 -0300

    fix: adicionado o método de voltar e ajustado o responsivo na classe privacy

--- src/components/Privacy/index.tsx
+++ src/components/Privacy/index.tsx
@@ -7,38 +7,42 @@ import "../Privacy/privacy.scss";
 import "../Preferences/Preferences.scss";
 import { Link } from "react-router-dom";
 
-
 const Privacy = () => {
     return (
         <>
             <div className="options-container container-privacy">
                 <h1 id="titulo" className="titulo-privacy">Privacidade</h1>
-                <Link to="app/options/privacy">
-                    <div className="option-card minha-conta reset-password">
-                        <MdLockReset className="icone-reset-password">
-                        </MdLockReset>
-                        <h3>Resetar senha
-                            <h6> Isso apagará todas as informações da conta. Voltará ao estado inicial.</h6>
-                        </h3>
+                <div className="div-componentes-privacy">
+                    <Link to="/app/options/privacy">
+                        <div className="option-card minha-conta reset-password">
+                            <MdLockReset className="icone-reset-password" />
+                            <h3>Resetar senha
+                                <h6>
+                                    Isso apagará todas as informações da conta. Voltará ao estado inicial.
+                                </h6>
+                            </h3>
+                        </div>
+                    </Link>
 
-                    </div>
-                </Link>
+                    <Link to="/app/options/privacy">
+                        <div className="option-card minha-conta reset-account">
+                            <MdDelete className="icone-reset-password" />
+                            <h3>Resetar senha
+                                <h6>
+                                    Isso apagará sua conta de forma permanente.
+                                </h6>
+                            </h3>
+                        </div>
+                    </Link>
 
-                <Link to="app/options/privacy">
-                    <div className="option-card minha-conta reset-password">
-                        <MdDelete className="icone-reset-password">
-                        </MdDelete>
-                        Apagar Conta
-                    </div>
-                </Link>
+                    <Link to="app/options/privacy">
+                        <div className="option-card minha-conta buttom-back">
+                            <GrLogout className="icone-reset-password" />
+                            Voltar
+                        </div>
+                    </Link>
 
-                <Link to="app/options/privacy">
-                    <div className="option-card minha-conta reset-password">
-                        <GrLogout className="icone-reset-password">
-                        </GrLogout>
-                        Voltar
-                    </div>
-                </Link>
+                </div>
 
             </div>
 
