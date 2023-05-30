c81841d9ad2c2b57250003c9c2573607f67b4ef9
andersonnevesfac <nevesgamer27@gmail.com>
2023-05-30 00:05:21 -0300

    Fix: Adicionado o modal e retorno das rotas através de uma função nativa do DOM

--- src/components/Privacy/index.tsx
+++ src/components/Privacy/index.tsx
@@ -1,6 +1,6 @@
 import { BiCategory } from "react-icons/bi";
 import { GrLogout } from "react-icons/gr";
-import { ImCoinDollar } from "react-icons/im";
+import { ImWarning } from "react-icons/im";
 import { MdLockReset, MdDelete } from "react-icons/md";
 import "../Privacy/privacy.scss";
 import { useState } from "react";
@@ -15,28 +15,34 @@ const Privacy = () => {
         <>
             <div className="options-container container-privacy">
                 <h1 id="titulo" className="titulo-privacy">Privacidade</h1>
-                
-                    <div className="option-card  " onClick={() => setShow(true)} >
-                        <MdDelete className="icone-reset-password"/>                    
-                        <h3>Apagar conta
-                            <h6> Isso apagará todas as informações da conta. Voltará ao estado inicial.</h6>
-                        </h3>
-                    </div>                        
-                
-                    <div className="option-card  reset-password">
-                        <MdLockReset className="icone-reset-password"/>                        
-                        Resetar Senha
-                    </div>        
-                
-                    <div className="option-card voltar"onClick={() =>{window.history.back()}}>
-                        <GrLogout className="icone-reset-password">
-                        </GrLogout>
-                        Voltar
-                    </div>                
+
+                <div className="option-card  " onClick={() => setShow(true)} >
+                    <MdDelete className="icone-reset-password" />
+                    <h3>Apagar conta
+                        <h6> Isso apagará todas as informações da conta. Voltará ao estado inicial.</h6>
+                    </h3>
+                </div>
+
+                <div className="option-card  reset-password">
+                    <MdLockReset className="icone-reset-password" />
+                    Resetar Senha
+                </div>
+
+                <div className="option-card voltar" onClick={() => { window.history.back() }}>
+                    <GrLogout className="icone-reset-password">
+                    </GrLogout>
+                    Voltar
+                </div>
             </div>
 
-            <Modal show={show} onClose={()=>{setShow(false)}} >
-                <h1 id="titulo-modal">Apagar sconta</h1>
+            <Modal show={show} onClose={() => { setShow(false) }} >
+                <div className="reset-account-modal">
+                    <h1 id="titulo-modal">Apagar Conta</h1>
+                    <div>
+                        <ImWarning className="icone-warning-privacy"></ImWarning>
+                    </div>
+                </div>
+
             </Modal>
 
         </>
