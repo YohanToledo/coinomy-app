74982383b86738a21e3df15096d893ac9b4ce775
ANDERSON DOS SANTOS NEVES <ASNEVES2@UNIVEL.UNIVEL.BR>
2023-05-29 21:51:16 -0300

    Fix: Realizado ajustes nas classes e links adicionais, adicionado a função de TS/JS windows.history.back()

--- src/components/Privacy/index.tsx
+++ src/components/Privacy/index.tsx
@@ -3,75 +3,41 @@ import { GrLogout } from "react-icons/gr";
 import { ImCoinDollar } from "react-icons/im";
 import { MdLockReset, MdDelete } from "react-icons/md";
 import "../Privacy/privacy.scss";
+import { useState } from "react";
 
 import "../Preferences/Preferences.scss";
 import { Link } from "react-router-dom";
+import Modal from "../Modal";
 
 const Privacy = () => {
+    const [show, setShow] = useState(false);
     return (
         <>
             <div className="options-container container-privacy">
                 <h1 id="titulo" className="titulo-privacy">Privacidade</h1>
-<<<<<<< HEAD
-                <Link to="app/options/privacy">
-                    <div className="option-card minha-conta reset-password">
-                        <MdLockReset className="icone-reset-password"/>                    
-                        <h3>Resetar senha
+                
+                    <div className="option-card  " onClick={() => setShow(true)} >
+                        <MdDelete className="icone-reset-password"/>                    
+                        <h3>Apagar conta
                             <h6> Isso apagará todas as informações da conta. Voltará ao estado inicial.</h6>
                         </h3>
-                    </div>
-                </Link>
-        
-                <Link to="app/options/privacy">
-                    <div className="option-card minha-conta reset-password">
-                        <MdDelete className="icone-reset-password">
-                        </MdDelete>
-                        Apagar Conta
-                    </div>
-                </Link>
-
-                <Link to="/app/options">
-                    <div className="option-card minha-conta reset-password">
+                    </div>                        
+                
+                    <div className="option-card  reset-password">
+                        <MdLockReset className="icone-reset-password"/>                        
+                        Resetar Senha
+                    </div>        
+                
+                    <div className="option-card voltar"onClick={() =>{window.history.back()}}>
                         <GrLogout className="icone-reset-password">
                         </GrLogout>
                         Voltar
-                    </div>
-                </Link>
-=======
-                <div className="div-componentes-privacy">
-                    <Link to="/app/options/privacy">
-                        <div className="option-card minha-conta reset-password">
-                            <MdLockReset className="icone-reset-password" />
-                            <h3>Resetar senha
-                                <h6>
-                                    Isso apagará todas as informações da conta. Voltará ao estado inicial.
-                                </h6>
-                            </h3>
-                        </div>
-                    </Link>
-
-                    <Link to="/app/options/privacy">
-                        <div className="option-card minha-conta reset-account">
-                            <MdDelete className="icone-reset-password" />
-                            <h3>Resetar senha
-                                <h6>
-                                    Isso apagará sua conta de forma permanente.
-                                </h6>
-                            </h3>
-                        </div>
-                    </Link>
-
-                    <Link to="app/options/privacy">
-                        <div className="option-card minha-conta buttom-back">
-                            <GrLogout className="icone-reset-password" />
-                            Voltar
-                        </div>
-                    </Link>
-
-                </div>
->>>>>>> 6b7b175716ed086155f35d15ddb31bbaee8cf167
-
+                    </div>                
             </div>
+
+            <Modal show={show} onClose={()=>{setShow(false)}} >
+                <h1 id="titulo-modal">Apagar sconta</h1>
+            </Modal>
 
         </>
     )
